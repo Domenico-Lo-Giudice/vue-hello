ce55e35fd6b714360e82d1762436229d8ced6718
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-10 09:55:34 +0100

    add bouns

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            image: "./img/d3de2d626c552b1294f172712937c0e5.jpg"
         }
     }
 });
